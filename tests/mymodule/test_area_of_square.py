a916b969ee9ab91537838e5f2bdcdab8b71bd4f7
zanfack25 <zanfackdave@gmail.com>
2025-10-18 16:16:30 -0400

    Fix expected output for test case of area_of_square

--- tests/mymodule/test_area_of_square.py
+++ tests/mymodule/test_area_of_square.py
@@ -3,7 +3,7 @@ import pytest
 from myapp.mymodule.funcs import area_of_square 
 
 
-@pytest.mark.parametrize("a, b", [(38,1444),(10,100), (20,400), (11,121), (38,1444)])
+@pytest.mark.parametrize("a, b", [(38,1444),(10,100), (20,400), (11,121), (38,1456)])
 def test_area_of_square(a, b):
     res = area_of_square(a)
     assert res == b
